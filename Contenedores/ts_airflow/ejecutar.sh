013c6bd9370bfe320544dd264ce7736eda339005
Omar Velez <omargo33@gmail.com>
2026-06-28 12:17:34 -0500

    Up [Ov] Configuracion de ts_ariflow

--- Contenedores/ts_airflow/ejecutar.sh
+++ Contenedores/ts_airflow/ejecutar.sh
@@ -1,8 +1,25 @@
+if [ ! -d "./dags" ]; then
+  echo "No estan instaladas las carpetas del producto, por favor usar ./instalar.sh"
+  exit 1
+fi 
+
+
 docker compose build
 docker compose up -d
 
-while ! curl -s http://localhost:8080 > /dev/null 2>&1; do
-  echo "Esperando a que airflow al puerto 8080"
-  sleep 2
-done
-echo "Airflow ya está activo en http://localhost:8080"
+animacion() {
+  local caracteres='|/-\'
+
+  printf "Esperando sitio activo: "
+  while ! curl -s http://localhost:8080 > /dev/null 2>&1; do
+    for i in $(seq 0 3); do
+      printf "%s" "${caracteres:$i:1}"
+      sleep 0.1
+      printf "\b"
+    done
+    #sleep 30
+  done
+  echo "Airflow ya está activo en http://localhost:8080"
+}
+
+animacion
--- Contenedores/ts_airflow/instalar.sh
+++ Contenedores/ts_airflow/instalar.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
+# instalar.sh
+
+# Crear carpetas
 mkdir -p dags logs plugins config
-sudo chown -R 50000:0 dags logs plugins config
+
+# Cambiar propietario al usuario actual usando su UID real
+sudo chown -R $(id -u):$(id -g) dags logs plugins config
+
+# Dar permisos adecuados
+chmod -R 755 dags logs plugins config
+
+echo "Carpetas creadas con permisos para el usuario $(whoami) (UID: $(id -u))"
